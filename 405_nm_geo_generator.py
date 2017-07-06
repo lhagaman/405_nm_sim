4e12fab276eae998a455346d7f729cba01518661
Lee Hagaman <lhagaman@berkeley.edu>
2017-07-06 12:26:09 -0700

    started setting up geometry, figured out how to view the geometry and made a script to copy the geo file to the appropriate place.

--- 405_nm_geo_generator.py
+++ 405_nm_geo_generator.py
@@ -10,35 +10,43 @@ geoFileName = './405_nm.geo'
 tube_included = True
 # 'air', 'water', 'mineraloil'
 substance = "air"
+# incident angle of beam with respect to sample in degrees
+theta_i = "45"
 
+tube_radius = 1.75 / 2
+tube_wall_width = 0.125
+tube_height = 2
+
+sample_radius = 0.5
+sample_thickness = 0.175
 
 masterString = ''
 
 ##### Create the world first
 world = GS.boxVolume('world', 400, 400, 400)
-world.material = 'acrylic_black'
+world.material = 'air'
 world.mother = ''
 world.invisible = 1
 masterString = world.writeToString(masterString)
 
-
-
-# air inside world
-
-# sapphire tube
-
-# air inside sapphire tube
+if tube_included:
+    # sapphire tube
+    sapphire_tube = GS.tubeVolume("sapphire_tube", tube_radius, tube_height, tube_radius - tube_wall_width)
+    sapphire_tube.center = {'x':0, 'y':0, 'z':0}
+    sapphire_tube.mother = 'world'
+    sapphire_tube.material = 'sapphire'
+    masterString = sapphire_tube.writeToString(masterString)
 
 # sample
+sample = GS.tubeVolume('sample', sample_radius, sample_thickness)
+sample.center = {'x':0, 'y':0, 'z':0}
+sample.rotation = [45, 90, 0]
+sample.mother = 'world'
+sample.material = "teflon"
+masterString = sample.writeToString(masterString)
 
-# vaccuum shell cylinder to check for reflected entering photons
+# vaccuum shell cylinder to check for reflected entering photons ?
 
-
-
-
-
-##### Write geo out to file.
-#print masterString
 
 print 'Saving file...'
 geoOutFile = open(geoFileName,'w+')
